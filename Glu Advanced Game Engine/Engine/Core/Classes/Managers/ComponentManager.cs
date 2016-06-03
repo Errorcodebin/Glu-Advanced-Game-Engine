4f9c9926505e7dcfd4bcdd73e5f29017ce366069
Errorcodebin <Errorcodebin_2@hotmail.com>
2016-06-03 12:50:13 +0200

    Attempt at making a Component manager, trying to apply LinQ in this to learn its purpose.

---
+++ Glu Advanced Game Engine/Engine/Core/Classes/Managers/ComponentManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace GameEngine.Core
+{
+    class ComponentManager
+    {
+        public List<Component> m_components = new List<Component>();
+
+        public void addComponent(Component component)
+        {
+            m_components.Add(component);
+        }
+
+        public t getComponent<t>(Component component) where t : Component
+        {
+            t result = (t)from c in m_components where c.GetType() == typeof(t) select c;
+
+            if (result != null) return result;
+            else return default(t);
+        }
+
+        public void Update()
+        {
+            foreach (Component C in m_components)
+            {
+
+            }
+        }
+
+        public void Draw();
+    }
+}
