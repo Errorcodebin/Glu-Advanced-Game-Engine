aa1c1c76f9c5509ec33d776e72abc0283106b78d
Errorcodebin <Errorcodebin_2@hotmail.com>
2016-06-03 10:44:17 +0200

    Continued Implementing Transform Component

--- Glu Advanced Game Engine/Engine/Essentials/Components/Transform.cs
+++ Glu Advanced Game Engine/Engine/Essentials/Components/Transform.cs
@@ -12,29 +12,48 @@ namespace GameEngine
         private Vector2 m_DeltaPosition;
         private Vector2 m_PreviousPosition;
         private Vector2 m_Scale;
+        private float m_Rotation;
 
         public Vector2 position
         {
             get { return m_DeltaPosition; }
-            set {  m_DeltaPosition = value;}
+            set { m_DeltaPosition = value; }
         }
 
         public Vector2 previousPosition
         {
-            get;
-            set;
+            get { return m_PreviousPosition; }
+            private set { m_PreviousPosition = value; }
         }
 
-        private float m_Rotation;
+
+        public Vector2 scale
+        {
+            get { return m_Scale; }
+            private set { m_Scale = value; }
+        }
+        public void Rotate(float degrees)
+        {
+            m_Rotation += degrees;
+        }
 
         public void Translate(Vector2 translation)
         {
-            m_Position += translation;
+            m_DeltaPosition += translation;
         }
 
-        protected override void Update()
+        protected override void Awake()
         {
+            m_DeltaPosition = Vector2.zero;
+            m_Position = Vector2.zero;
+            m_Scale = new Vector2(1, 1);
+            m_Rotation = 0f;
+        }
 
+        protected override void UpdateLate()
+        {
+            m_PreviousPosition = m_Position;
+            m_Position = m_DeltaPosition;
         }
     }
 }
