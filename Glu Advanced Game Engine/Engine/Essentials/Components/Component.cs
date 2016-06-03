b7ddada65e3558b73576ba88ee14683d2477bbbb
Errorcodebin <Errorcodebin_2@hotmail.com>
2016-06-03 09:26:33 +0200

    Added "Early" and "Late" update and paint for convenience.

--- Glu Advanced Game Engine/Engine/Essentials/Components/Component.cs
+++ Glu Advanced Game Engine/Engine/Essentials/Components/Component.cs
@@ -4,12 +4,20 @@ namespace GameEngine
 {
     class Component
     {
+        /* TODO implement
         public Transform transform;
         public BaseBehavior gameObject;
-
+        */
+        public virtual void UpdateEarly() { }
         public virtual void Update() { }
+        public virtual void UpdateLate() { }
+
         public virtual void Start() { }
+
+        public virtual void PaintEarly() { }
         public virtual void Paint() { }
+        public virtual void PaintLate() { }
+
         public virtual void Destroy() { }
         public virtual void OnComponentAdd(Component component) { }
         public virtual void OnComponentRemove(Component component) { }
